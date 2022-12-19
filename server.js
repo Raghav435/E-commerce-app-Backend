739f2079539a06cde2dd0424b77a30a955cdbbcd
Raghav Shukla <shuklaraghav435@gmail.com>
2022-12-19 19:40:04 +0530

    added

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 require("dotenv").config();
 const connect = require("./config/db");
+var cors = require("cors");
+const jwt = require("jsonwebtoken");
+const productController = require("./controllers/product.controller");
+
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/fraazo", productController);
+app.use("/user", userController);
+app.use("/payment", paymentController);
 
 const port = process.env.PORT || 4040;
 app.listen(port, async () => {
