60cfc44a4723759addb70a62b104c2580ff987a7
Raghav Shukla <shuklaraghav435@gmail.com>
2022-12-19 22:18:56 +0530

    payment gateway added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const productController = require("./controllers/product.controller");
 const userRouter = require("./routes/user.route");
 const userController = require("./controllers/user.controller");
-
+const paymentController = require("./controllers/payment.controller");
 
 const app = express();
 app.use(cors());
@@ -19,14 +19,35 @@ app.use(express.json());
 // });
 app.use("/product", productController);
 app.use("/", userRouter);
-app.use("/user", userController)
+app.use("/user", userController);
+app.use("/payment", paymentController);
+
+// API for PAYMENT
+
+app.post("/payment/create", async (req, res) => {
+  let total = req.body.amount;
+  console.log("Payment Request recieved for this ruppess", total,typeof(total));
+
+  total = Number(total);
+const paymentIntent = await stripe.paymentIntents.create({
+  amount: 100,
+  currency: "inr",
+  payment_method_types: ["card"],
+});
+
+  console.log(paymentIntent)
+
+  res.status(201).send({clientSecret: paymentIntent.client_secret});
+});
 
 const port = process.env.PORT || 4040;
 app.listen(port, async () => {
   try {
     await connect();
-    console.log(`Successfully connected to database & server is running on ${port}`) ;
+    console.log(
+      `Successfully connected to database & server is running on ${port}`
+    );
   } catch (error) {
-    console.log("error:", error) ;
+    console.log("error:", error);
   }
 });
